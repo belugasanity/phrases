73af2e761fd95549e2d9c1c35f55e963e66ba419
Austin <aust.bagley@gmail.com>
2024-05-20 18:29:15 -0600

    add get sheet data method

--- src/app/google-sheets.service.ts
+++ src/app/google-sheets.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleSheetsService {
+  private apiKey: string = environment.sheetsAPI;
+  private spreadsheetId: string = environment.sheetId;
+  private range: string = 'Timeline: Phrases';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSheetData(): Observable<any[]> {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${this.range}?key=${this.apiKey}`;
+    return this.http.get<any>(url).pipe(
+      map(response => response.values)
+    );
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  sheetsAPI: "AIzaSyB69DJ_3oyNLumesFOht9ekuZ0D-IjZNCY",
+  sheetsAPI: 'AIzaSyB69DJ_3oyNLumesFOht9ekuZ0D-IjZNCY',
+  sheetId: '1xNdVjR_n5z-8kLB7iNuLCeZvK5XCpId0d8XiTeCofGI',
 }
